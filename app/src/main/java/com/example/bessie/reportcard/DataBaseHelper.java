3a3771f753d0c52c40bea4722049dfd1dd6ac488
Bessie <thibedibbt@gmail.com>
2016-10-31 08:26:34 +0000

    1st commit

--- app/src/main/java/com/example/bessie/reportcard/DataBaseHelper.java
+++ app/src/main/java/com/example/bessie/reportcard/DataBaseHelper.java
@@ -17,6 +17,13 @@ public class DataBaseHelper extends SQLiteOpenHelper {
     public static final String COL_2 = "NAME";
     public static final String COL_3 = "SURNAME";
     public static final String COL_4 = "MARKS";
+    public static final String TABLE_NAME2 = "register_table";
+    public static final String COL2_1 = "ID";
+    public static final String COL2_2 = "name";
+    public static final String COL2_3 = "username";
+    public static final String COL2_4 = "password";
+    public static final String COL2_5 = "numbers";
+
 
 
     public DataBaseHelper(Context context) {
@@ -27,12 +34,16 @@ public class DataBaseHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
 
         db.execSQL("create table " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT , SURNAME TEXT, MARKS INTEGER)" );
+        db.execSQL("create table " + TABLE_NAME2 + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT , USERNAME TEXT, MARKS INTEGER, PASSWORD INTEGER, NUMBERS INTEGER)" );
+
 
     }
+
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME );
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME2 );
         onCreate(db);
 
     }
@@ -54,6 +65,26 @@ public class DataBaseHelper extends SQLiteOpenHelper {
 
     }
 
+    public boolean insertData2 (String name, String username, int numbers, int password){
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL2_2,name);
+        contentValues.put(COL2_3,username);
+        contentValues.put(COL2_4,numbers);
+        contentValues.put(COL2_5,password);
+        long results = db.insert(TABLE_NAME2,null,contentValues);
+
+        if (results == -1)
+            return false;
+        else
+            return true;
+
+
+
+
+    }
+
     public Cursor getAllData(){
 
         SQLiteDatabase db = this.getWritableDatabase();
@@ -62,6 +93,17 @@ public class DataBaseHelper extends SQLiteOpenHelper {
 
 
     }
+
+    public Cursor getAllData2(){
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor c = db.rawQuery("select * from "+TABLE_NAME2,null );
+        return c;
+
+
+    }
+
+
 
     public boolean updateData(String id, String name, String surname, String marks){
 
--- app/src/main/java/com/example/bessie/reportcard/RegisterActivity.java
+++ app/src/main/java/com/example/bessie/reportcard/RegisterActivity.java
@@ -2,22 +2,31 @@ package com.example.bessie.reportcard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
+    DataBaseHelper myDb;
+     Button registerbtn;
+    EditText etname , etusername ,etpassword , etnumbers ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        final EditText etAge = (EditText)findViewById(R.id.etAge);
-        final EditText etName = (EditText)findViewById(R.id.etName);
-        final EditText etUsername = (EditText)findViewById(R.id.etUsername);
-        final EditText etPassword = (EditText)findViewById(R.id.etPassword);
-
-        final Button bRegister = (Button)findViewById(R.id.registerbtn);
+     registerbtn = (Button)findViewById(R.id.registerbtn);
+        etname = (EditText)findViewById(R.id.etName);
+        etnumbers =(EditText)findViewById(R.id.etnumbers);
+        etusername=(EditText) findViewById(R.id.etUsername);
+        etpassword=(EditText)findViewById(R.id.etPassword);
 
     }
+
+
+
+
+
 }
