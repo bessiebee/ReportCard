f4180be95afc10472d20a92ad13a304cf834bb34
Bessie <thibedibbt@gmail.com>
2016-11-21 09:33:36 +0000

    1st commit

---
+++ app/src/main/java/com/example/bessie/reportcard/contact.java
@@ -0,0 +1,60 @@
+package com.example.bessie.reportcard;
+
+/**
+ * Created by Bessie on 04/11/2016.
+ */
+public class contact {
+
+    int id;
+    String name, email, username,password;
+
+    public void setId(int id){
+
+        this.id=id;
+
+    }
+public int getId(){
+
+    return this.id;
+
+}
+    public void setName(String name){
+
+        this.name=name;
+
+    }
+public String  getname(){
+
+    return this.name;
+}
+    public void setUsername(String username){
+
+        this.username=username;
+
+    }
+    public String  getUsername (){
+
+        return this.username;
+    }
+
+    public void setEmail(String email){
+
+        this.email=email;
+
+    }
+public String getEmail(){
+
+    return this.email;
+}
+public void setPassword(String password){
+
+    this.password=password;
+
+}
+
+    public String getPassword(){
+
+        return this.password;
+    }
+
+}
